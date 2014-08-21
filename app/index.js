d26dec01993fe872e8f62ec08cc53fc58a8e35b1
David Schwertfeger <david.schwertfeger@googlemail.com>
2014-08-21 12:15:48 +0200

    Update default phaser version to 2.0.7

--- app/index.js
+++ app/index.js
@@ -28,7 +28,7 @@ var PhaserGenerator = yeoman.generators.Base.extend({
     {
       name: 'phaserVersion',
       message: 'Which Phaser version would you like to use?',
-      default: '2.0.6'
+      default: '2.0.7'
     },
     {
       name: 'gameWidth',
